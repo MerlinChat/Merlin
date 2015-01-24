ec7379f62bba126a02e8f0f0d072baa0fcdbbc78
Rohit Mulange <rohit@rohit.codes>
2015-01-24 22:57:56 +0530

    Roster Basic Implementation
    Signed-off-by: Rohit Mulange <rohit@rohit.codes>

--- main.js
+++ main.js
@@ -59,7 +59,148 @@ var xmpp={
 			
 			xmpp.send(text)
 		}
-	}
+	},
+
+	fetchRoster:function(customHandler){
+
+		if(!(customHandler)) {
+			handler=xmpp.defaultHandlers.roster
+		}
+		else {
+
+			handler=function(iq){
+
+				xmpp.defaultHandlers.roster(iq)
+				customHandler(iq)
+			}
+		}
+		var iq=$iq({type:'get'}).c('query',{xmlns:'jabber:iq:roster'})
+		xmpp.sendIQ(iq,handler)
+
+	},
+	
+	sendIQ:function(iq,handler) {
+	
+		xmpp.conn.sendIQ(iq,handler)
+	},
+
+	setupHandlers:function(){
+		
+		//Presence
+
+		var presenceHandler=function(presence){
+
+			if(xmpp.customHandlers.presence){
+
+				
+				if(!(xmpp.customHandlers.presence.override)){
+
+					xmpp.defaultHandlers.presence(presence)
+				}
+				xmpp.customHandlers.presence.handler(presence)
+			}
+			else {
+				xmpp.defaultHandlers.presence(presence)
+			}
+		}
+
+		xmpp.conn.addHandler(presenceHandler,null,'presence')
+		
+
+		//IQ
+
+
+
+
+		//Message
+	},
+
+	defaultHandlers:{
+
+		roster:function(iq) {
+
+			//Default Roster Handler. 
+			//This roster is not registered using addHandler but used dynamically for sendIQ
+
+			console.log(iq)
+			$(iq).find('item').each(function(){
+				var contact={}
+				contact.jid=$(this).attr('jid')
+				contact.name=$(this).attr('name') || contact.jid
+				contact.jid=Strophe.getBareJidFromJid(contact.jid)
+				contact.presence='offline'
+				xmpp.roster[contact.jid]=contact
+
+			})
+		},
+
+		
+		presence:function(presence){
+
+			//Default Presence handler updates the presence of default roster only
+			//Registerd using addHandler
+
+			var type=$(presence).attr('type')
+			var from=$(presence).attr('from')
+			if(type!=='error') {
+				var status='offline'
+				var jid=Strophe.getBareJidFromJid(from)
+				if(xmpp.roster[jid]) {
+
+					if(type!=='unavailable') {
+
+						var show=$(presence).find('show').text()
+						if(show==''||show=='chat'){
+
+							status='online'
+						} 
+						else {
+
+							status='away'
+						}
+
+					}
+					xmpp.roster[jid].status=status
+				}
+			}
+		},
+
+		iq:function(){
+
+			//Do Nothing
+			//not is use currently
+		}
+
+	},
+
+	customHandlers:{},
+
+	//RegisteCustomHandler
+
+	registerHandler:function(type,handler,override){
+
+		if(!(override)) {
+			override=false
+		}
+		xmpp.customHandlers[type]={
+
+			handler:handler,
+			override:override
+		}
+	},
+
+
+
+	//Return System maintained Roster
+
+	getDefaultRoster:function(){
+
+		return xmpp.roster
+	},
+	
+	//System maintained roster, requires use od default handlers for proper update
+
+	roster:{},
 
 }
 
@@ -110,6 +251,11 @@ app.controller("mainController",function($scope,$rootScope){
 		window.location.href='#/'
 
 	})
+	$scope.$on("statusChange",function (event,args){
+
+		console.log(args.status)
+	})
+
 
 })
 
@@ -161,6 +307,7 @@ app.controller("loginController",function($scope,$rootScope){
 
 					$rootScope.$broadcast("statusChange", {status:"Connected"})
 					$rootScope.$broadcast("loginSuccess", { username:$scope.loginFormData.username})
+					xmpp.fetchRoster()
 
 
 				}
@@ -171,7 +318,6 @@ app.controller("loginController",function($scope,$rootScope){
 					$rootScope.$broadcast("logout", { })
 					
 				}
-				
 				
 
 			})
@@ -218,13 +364,17 @@ app.controller("dashboardController",function($scope,$rootScope){
 	}
 
 
+	xmpp.registerHandler('presence',function (presence){
+		$rootScope.$broadcast('rosterUpdate',{})
+	})
+
+	xmpp.setupHandlers()
 	xmpp.presence()
 	
 	$scope.consoleLogs=[]
 	
 	$scope.show_traffic=function (body,type) {
 
-		console.log($scope.consoleLogs)
 		$scope.consoleLogs.push({body:Strophe.serialize(body),type:type})
 		$scope.$apply()	
 	}
@@ -241,17 +391,19 @@ app.controller("dashboardController",function($scope,$rootScope){
 		}
 
 	}
-})
+	$scope.roster=xmpp.getDefaultRoster()
 
+	$scope.getRoster=function(){
 
+		return $scope.roster;
+	}
+	$scope.$on("rosterUpdate",function (event,body){
 
-app.controller("logController",function($scope) {
-	
-	$scope.logs=[]
+		$scope.roster=xmpp.getDefaultRoster()
+		$scope.apply()
 
-	$scope.$on("statusChange",function (event,args){
-
-		$scope.logs.push(args.status)
 	})
 
 })
+
+
